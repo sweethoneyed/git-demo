c7a1aba48d2694d73949b08298626f448432360a
sweethoneyed <1422586639@qq.com>
2020-01-08 10:31:35 +0800

    试试

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/login.vue'
+import User from '../components/user/user.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+  { path: '/user', component: User }
+
 ]
 
 const router = new VueRouter({
